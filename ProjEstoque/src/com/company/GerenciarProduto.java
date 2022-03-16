b629905823be8f8102d4e8da06fb680c82da6378
Tarciso Bandeira <42774157+tarcisobandeira@users.noreply.github.com>
2022-03-15 21:10:00 -0300

    Add files via upload

---
+++ ProjEstoque/src/com/company/GerenciarProduto.java
@@ -0,0 +1,87 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class GerenciarProduto {
+
+    static Produto p = new Produto();
+    static Scanner sn = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int opt = 0;
+        do {
+            System.out.println("Menu Principal");
+            System.out.println("1. Cadastrar Produto");
+            System.out.println("2. Registrar Entrada");
+            System.out.println("3. Registrar Saida");
+            System.out.println("4. Consultar");
+            System.out.println("5. Apresentar Nivel");
+            System.out.println("6. Sair");
+            System.out.println("\nSua opcao: ");
+
+            opt = Integer.parseInt(sn.nextLine());
+
+            switch (opt){
+                case 1:
+                    execCadastrar();
+                    break;
+                case 2:
+                    execDarEntrada();
+                    break;
+                case 3:
+                    execDarSaida();
+                    break;
+                case 4:
+                    exercConsultar();
+                    break;
+                case 5:
+                    execMostrarNivel();
+                    break;
+                case 6:
+                       break;
+                default:
+                    System.out.println("Essa opcao nao existe.");
+                    break;
+            }
+        } while (opt != 6);
+    }
+
+    public static void execCadastrar(){
+        System.out.println("Digite o ID do Produto: ");
+        p.idProduto = Integer.parseInt(sn.nextLine());
+        System.out.println("Digite a Descricao do produto: ");
+        p.descricao = sn.nextLine();
+        System.out.println("Digite a quantidade no estoque: ");
+        p.quantidade = Long.parseLong(sn.nextLine());
+        System.out.println("Digite o nivel para o estoque: ");
+        p.nivel = Long.parseLong(sn.nextLine());
+        System.out.println("Digite o valor do produto: ");
+        p.valor = Double.parseDouble(sn.nextLine());
+        System.out.println("Produto cadastrado.");
+    }
+
+    public static void execDarEntrada(){
+        if(p != null){
+            System.out.println("Digite o valor para a entrada do estoque: ");
+            long l = (Long.parseLong(sn.nextLine()));
+            if(p.entrada(l)){
+                System.out.println("Quantidade: " + l + " foi adicionada ao produto " + p.descricao + ". Estoque atual: " + p.quantidade);
+            }else{
+                System.out.println("");
+            }
+        }
+        System.out.println("Voce nao cadastrou o produto.");
+    }
+
+    public static void execDarSaida(){
+
+    }
+
+    public static void execMostrarNivel(){
+        p.checarNivel();
+    }
+
+    public static void exercConsultar(){
+        p.imprimir();
+    }
+}
---
+++ ProjEstoque/src/com/company/Produto.java
@@ -0,0 +1,44 @@
+package com.company;
+
+public class Produto {
+    public int idProduto;
+    public String descricao;
+    public long quantidade;
+    public long nivel;
+    public double valor;
+
+    public boolean entrada(long l){
+        if(l > 0){
+            quantidade += l;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean saida(long l){
+        if(l <= quantidade && l > 0){
+            quantidade -= l;
+            return true;
+        }
+        return false;
+    }
+
+    public void imprimir(){
+        System.out.println("----------------------");
+        System.out.println("ID Produto: " + idProduto);
+        System.out.println("Descricao: " + descricao);
+        System.out.println("Quantidade: " + quantidade);
+        System.out.println("Nivel: " + nivel);
+        System.out.println("Valor: " + valor);
+        System.out.println("----------------------");
+    }
+
+    public void checarNivel(){
+        if(nivel < quantidade){
+            System.out.println("Nivel abaixo do disponivel, " +
+                    "avisar o setor de compras.");
+        }else{
+            System.out.println("Nivel OK.");
+        }
+    }
+}
